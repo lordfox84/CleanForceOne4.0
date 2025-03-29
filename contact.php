ab890f5a7e54e312243a391e671ad93bde349f47
BUDSPENCER\peter <peter.kontur@astratex.com>
2025-03-30 00:31:51 +0100

    Dotenv

--- contact.php
+++ contact.php
@@ -1,9 +1,13 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 require 'vendor/autoload.php';
 session_start();
